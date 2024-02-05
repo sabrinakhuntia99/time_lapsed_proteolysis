68e26221ca37a926088a41018c2a09ac96a78c5d
Sabrina Khuntia <96092401+sabrinakhuntia99@users.noreply.github.com>
2024-02-05 15:20:10 -0600

    Update edit_convex_hull.py

--- edit_convex_hull.py
+++ edit_convex_hull.py
@@ -13,20 +13,23 @@ structure = p.get_structure('AF-O60361-F1-model_v4',
 # Extract amino acid sequence from the first model and chain
 print(extract_aa_sequence(structure))
 
-# Extract all atoms
+# Extract all atomic coordinates
 print(extract_all_atoms(structure))
 
-# Extract backbone atoms
+# Extract backbone atomic coordinates
 points = extract_backbone_atoms(structure)
 print(points)
 
-# Extract peptide cleavage XYZ coordinates
+# Layer the structure using convex hull peeling method
+convex_hull_peeling(points)
+
+# Extract all peptide cleavage XYZ coordinates (based on time point)
 data_df = pd.read_csv("pep_cleave_coordinates_10292023.csv", index_col=0)
 data_df = data_df.applymap(conv_array_text)
 print(data_df)
 
-# Peel convex hulls
-convex_hull_peeling(points)
-
-
-
+# Extract specific time-lapsed peptide cleave XYZ coordinates for a given UniProt ID (ex: O60361)
+# Protein ID should match with that of the AlphaFold file
+search_value = "O60361"
+peptide_coord = data_df.loc[search_value]
+print(peptide_coord)
