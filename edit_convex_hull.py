122440d90fb82324f42e604c2e1d8247911a2d18
Sabrina Khuntia <96092401+sabrinakhuntia99@users.noreply.github.com>
2024-02-07 11:26:32 -0600

    Update edit_convex_hull.py

--- edit_convex_hull.py
+++ edit_convex_hull.py
@@ -1,8 +1,10 @@
 from __future__ import division
 from Bio.PDB import *
+import plotly.graph_objects as go
 import pandas as pd
-from util_func import conv_array_text, extract_aa_sequence, extract_all_atoms, extract_backbone_atoms
+from util_func import conv_array_text,conv_array_coord, extract_aa_sequence, extract_all_atoms, extract_backbone_atoms
 from calc_func import convex_hull_peeling
+from graph_func import plot_xyz_coordinates_with_time_and_hulls
 
 p = PDBParser()
 
@@ -26,10 +28,16 @@ convex_hull_peeling(points)
 # Extract all peptide cleavage XYZ coordinates (based on time point)
 data_df = pd.read_csv("pep_cleave_coordinates_10292023.csv", index_col=0)
 data_df = data_df.applymap(conv_array_text)
-print(data_df)
 
 # Extract specific time-lapsed peptide cleave XYZ coordinates for a given UniProt ID (ex: O60361)
 # Protein ID should match with that of the AlphaFold file
 search_value = "O60361"
 peptide_coord = data_df.loc[search_value]
 print(peptide_coord)
+
+conv_peptide_coord = conv_array_coord(peptide_coord)
+print(conv_peptide_coord)
+
+# Plot proteolysis data onto layered structure
+hull_layers = convex_hull_peeling(points)
+plot_xyz_coordinates_with_time_and_hulls(conv_peptide_coord, hull_layers)
